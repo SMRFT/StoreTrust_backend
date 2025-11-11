09cf03d271321dea6898bd4dff63baf64c102c83
dev/najma <najmasmrft@gmail.com>
2025-11-11 17:17:44 +0530

    db name change

--- storetrustbackend/database.py
+++ storetrustbackend/database.py
@@ -6,7 +6,7 @@ def get_database():
     # Get the database connection details from environment variables
     db_host = os.getenv('GLOBAL_DB_HOST', 'localhost')
     db_port = int(os.getenv('GLOBAL_DB_PORT', 27017))
-    db_name = os.getenv('GLOBAL_DB_NAME', 'StoreTrust')
+    db_name = os.getenv('STORETRUST_DB_NAME', 'StoreTrust')
 
     client = MongoClient(host=db_host, port=db_port)
     db = client[db_name]
